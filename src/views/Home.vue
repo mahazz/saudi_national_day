d2a7c7a66f3481da94b38339190d97d8f7cdcc3d
Maha Alzahrani <maha@Mahas-MacBook-Pro.local>
2020-09-05 11:56:36 +0300

    Import CountDown component into home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <CountDown />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import CountDown from "@/components/CountDown.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    CountDown
   }
 };
 </script>
